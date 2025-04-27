cf21ec0837e45dae2870ea4780385ace7734967b
PerSerMax <wundel.uber@yandex.ru>
2025-04-27 20:30:46 +0300

    Добавил openweather

--- actions/actions.py
+++ actions/actions.py
@@ -3,6 +3,25 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from datetime import datetime
 import pytz
+import requests
+
+def get_current_temperature(city: str, api_key: str) -> float:
+    base_url = "http://api.openweathermap.org/data/2.5/weather"
+    params = {
+        'q': city,
+        'appid': api_key,
+        'units': 'metric'  # для получения в °C
+    }
+
+    try:
+        response = requests.get(base_url, params=params)
+        data = response.json()
+        return data['main']['temp']
+    except Exception as e:
+        print(f"Ошибка при получении температуры: {e}")
+        return 0.0
+
+api_key = "8adfb3bf2336a9a8b4ade07984c8cdd6"  # Получите бесплатный ключ на https://openweathermap.org
 
 class ActionGetTime(Action):
     def name(self) -> Text:
@@ -47,7 +66,7 @@ class ActionGetTemperature(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         try:
-            current_temperature = 25  # Replace with actual temperature data
+            current_temperature = get_current_temperature("Москва", api_key)
             dispatcher.utter_message(text=f"Температура сейчас {current_temperature} градусов")
         except Exception as e:
             dispatcher.utter_message(text="Извините, не могу определить температуру")
